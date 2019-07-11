6399a6feac3f1380038c8e797e9234b203a1bba0
mKimp <ronsong@pdx.edu>
2019-07-11 02:29:57 -0700

    init

---
+++ test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+require('mocha')
+var assert = require('assert')
+var isEven = require('./')
+
+describe('isEven', function() {
+    it('should return true if the number is even:', function() {
+        assert(isEven(0))
+        assert(!isEven(1))
+        assert(isEven(2))
+        assert(!isEven(3))
+    })
+
+    it('should work with strings:', function() {
+        assert(isEven('0'))
+        assert(!isEven('1'))
+        assert(isEven('2'))
+        assert(!isEven('3'))
+    })
+
+    it('should throw an error on bad args:', function() {
+        assert.throws(function() {
+            isEven()
+        }, /expects a number\.$/)
+    })
+
+    it('should throw an error on non-integer args:', function() {
+        assert.throws(function() {
+            isEven('1.1e0')
+        }, /expects an integer\.$/)
+    })
+})
