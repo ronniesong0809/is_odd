c9cd1e84f3906e81916b98697ebbf2af15bcd1bb
mKimp <ronsong@pdx.edu>
2019-07-16 00:01:04 -0700

    fix: typo

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 const isNumber = require('is-number')
 
-module.exports = function isOdd(value) {
+module.exports = function isEven(value) {
     const n = Math.abs(value)
-    if (!isNumber(n)) {
+    if (!isNumber(n))
         throw new TypeError('expected a number')
-    }
-    if (!Number.isInteger(n)) {
+
+    if (!Number.isInteger(n))
         throw new Error('expected an integer')
-    }
-    if (!Number.isSafeInteger(n)) {
-        throw new Error('value exceeds maximum safe integer')
-    }
+
+    if (!Number.isSafeInteger(n))
+        throw new Error('expected a safe integer')
+
     return (n % 2) === 1
 }
